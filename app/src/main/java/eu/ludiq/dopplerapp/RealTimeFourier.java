2c1014e2b844a47048466da0f2fc18f6eba9d46a
Ludo Pulles <ludo.pulles@gmail.com>
2015-04-14 22:41:02 +0200

    Revert "Block size aangepast"
    This reverts commit c56f5101acba27b098e4b6e49103cdec44464f35.

--- app/src/main/java/eu/ludiq/dopplerapp/RealTimeFourier.java
+++ app/src/main/java/eu/ludiq/dopplerapp/RealTimeFourier.java
@@ -36,7 +36,7 @@ public class RealTimeFourier extends Activity {
     /**
      * deal with this many samples at a time
      */
-    private int blockSize = 256;
+    private int blockSize = 4096;
     /**
      * Sample rate in Hz
      */
