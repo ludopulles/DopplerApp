338bc71d7208c2d5ccd25d156cc883fbd6cc326c
LoekSteinebach <loek@steinebach.nl>
2015-04-14 16:31:56 +0200

    Knoppie

--- app/src/main/java/eu/ludiq/dopplerapp/MainActivity.java
+++ app/src/main/java/eu/ludiq/dopplerapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,6 +15,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AudioRecordTest.class));
+            }
+        });
     }
 
 
