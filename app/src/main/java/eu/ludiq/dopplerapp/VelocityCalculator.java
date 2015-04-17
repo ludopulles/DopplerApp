247cad266a30c99cfdcce3802ad823dcc71b337e
ludopulles <ludo.pulles@gmail.com>
2015-04-17 14:50:52 +0200

    Create VelocityCalculator.java

---
+++ app/src/main/java/eu/ludiq/dopplerapp/VelocityCalculator.java
@@ -0,0 +1,25 @@
+package eu.ludiq.dopplerapp;
+
+public class VelocityCalculator {
+  
+    private double speedOfSound;
+    
+    public VelocityCalculator() {
+        this.speedOfSound = 0.0;
+    }
+  
+    public void setSpeedOfSound(double speedOfSound) {
+        this.speedOfSound = speedOfSound;
+    }
+    
+    public double getSpeedOfSound() {
+        return this.speedOfSound;
+    }
+    
+    public double getSpeedOfObject(double frequencyApproaching, double frequencyLeaving) {
+        if (this.speedOfSound == 0.0) {
+            throw new IllegalStateException();
+        }
+        return (frequencyApproaching - frequencyLeaving) * this.speedOfSound / (frequencyApproaching + frequencyLeaving);
+    }
+}
